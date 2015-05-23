b6c6ecb8876346a4ba44c23c6a7b8a70e86ed228
josdem <joseluis.delacruz@gmail.com>
2015-05-23 15:42:36 -0500

    Eliminando persistence

--- persistence/src/main/groovy/com/tim/one/repository/StpLogTxRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tim.one.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.tim.one.model.StpLogTx;
-
-public interface StpLogTxRepository extends CrudRepository<StpLogTx, Integer> {
-}
