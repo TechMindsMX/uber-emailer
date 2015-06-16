29449ce46c8baf6306b8847866f1ad29e9630a97
josdem <joseluis.delacruz@gmail.com>
2015-06-16 10:15:15 -0500

    Modificando la url del servidor de cienhundreds

--- services/src/main/groovy/com/tim/one/state/ApplicationState.java
+++ services/src/main/groovy/com/tim/one/state/ApplicationState.java
@@ -2,7 +2,7 @@ package com.tim.one.state;
 
 public interface ApplicationState {
 	static final String WHITE_LIST = "white.list";
-	static final String HOST = "http://localhost:8080/cien-hundreds/";
+	static final String HOST = "http://cienhundreds.timone.mx/";
 	static final String FORGOT_PASSWORD_PREFIX = HOST + "recovery/show?token=";
 	static final String REGISTER_PREFIX = HOST + "recovery/confirm?token=";
 	static final String ADMIN_EMAIL = "joseluis.delacruz@gmail.com";
