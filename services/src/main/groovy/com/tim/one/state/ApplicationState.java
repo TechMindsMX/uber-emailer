e01a04c300da52023c551781064c3e2ef9528643
josdem <joseluis.delacruz@gmail.com>
2015-06-23 16:28:15 -0500

    Modificando el server de produccion

--- services/src/main/groovy/com/tim/one/state/ApplicationState.java
+++ services/src/main/groovy/com/tim/one/state/ApplicationState.java
@@ -2,7 +2,7 @@ package com.tim.one.state;
 
 public interface ApplicationState {
 	static final String WHITE_LIST = "white.list";
-	static final String HOST = "http://api.trama.mx/";
+	static final String HOST = "http://cienhundreds.techminds.com.mx/";
 	static final String FORGOT_PASSWORD_PREFIX = HOST + "recovery/show?token=";
 	static final String REGISTER_PREFIX = HOST + "recovery/confirm?token=";
 	static final String ADMIN_EMAIL = "remy.ochoa@techminds.com.mx";
